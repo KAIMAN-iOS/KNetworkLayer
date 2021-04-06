f88565c1fcd46ebf81d84e1c05083d78a020c439
jerometonnelier <jerometonnelier@yahoo.fr>
2021-04-06 08:52:31 +0200

    resolved

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/Alamofire/Alamofire", .branch("master")),
+        .package(url: "https://github.com/Alamofire/Alamofire", from: "5.4.2"),
         .package(url: "https://github.com/mxcl/PromiseKit", from: "6.0.0")
     ],
     targets: [
